8dc5d64067a3289275113886445d76228dcc73bd
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-06-29 21:31:36 -0300

    searching the header data of the tables

--- src/components/TablePanel/index.js
+++ src/components/TablePanel/index.js
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 const TablePanel = (props) => {
+  const tableName = props.tableName;
+  const [data, setData] = useState([]);
+  const [keysTable, setKeysTable] = useState([]);
+
+  useEffect(() => {
+    api.get('/alunos', {
+      headers: {
+        table_name: tableName
+      }
+    }).then(response => {
+      setData(response.data);
+    })
+    if(data[0]) {
+      setKeysTable(Object.keys(data[0]));
+    }
+  }, [data, tableName]);
+
   return (
     <div className="container-tablepanel">
-      <h2 className="title-panel">{ props.tableName }</h2>
+      <h2 className="title-panel">{ tableName }</h2>
       <table>
-        <tr>
-          <th>Campo1</th>
-          <th>Campo2</th>
-          <th>Campo3</th>
-          <th>Campo4</th>
-          <th>Campo1</th>
-          <th>Campo2</th>
-          <th>Campo3</th>
-          <th>Campo4</th>
-        </tr>
-        <tr>
-          <th>Campo1</th>
-          <th>Campo2</th>
-          <th>Campo3</th>
-          <th>Campo4</th>
-          <th>Campo1</th>
-          <th>Campo2</th>
-          <th>Campo3</th>
-          <th>Campo4</th>
-        </tr>
+        <thead>
+          <tr>
+            { keysTable.map((key, index) => (
+              <th key={index}>{ key }</th>
+            )) }
+          </tr>
+        </thead>
+        <tbody>
+          { data.map(object => (
+            <tr>
+              <td key={object.id}>{ object.id }</td>
+              <td key= {object.raça}>{ object[1] }</td>
+            </tr>
+          )) }
+        </tbody>
       </table>
     </div>
   )
